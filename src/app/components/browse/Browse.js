060e2896ed22cce98dbb44762f2031a63ba32063
Maxime BINET <mbinet@e1r4p18.42.fr>
2017-02-16 18:53:44 +0100

    add some filters

--- src/app/components/browse/Browse.js
+++ src/app/components/browse/Browse.js
@@ -8,6 +8,7 @@ import cookie from 'react-cookie';
 import { browserHistory } from "react-router";
 import {Link} from "react-router";
 import UserCard from './UserCard';
+import _ from 'lodash';
 
 
 
@@ -16,7 +17,12 @@ export class Browse extends React.Component {
     constructor() {
         super();
         this.state = {
-            users: []
+            users: [],
+            initialUsers: [],
+            filters: {
+                age1: "",
+                age2: ""
+            }
         };
     }
 
@@ -29,9 +35,79 @@ export class Browse extends React.Component {
             .then((response) => {
                 console.log(response.body.users)
                 this.setState({
-                    users: response.body.users
+                    users: response.body.users,
+                    initialUsers: response.body.users
                 })
             });
+    }
+
+    sortBy(what) {
+        if (what == 'location') {
+            this.componentWillMount()
+        }
+        else if (what == 'tags') {
+            var user = cookie.load('user');
+
+            if (user.tags) {
+                var copy = _.map(this.state.users, _.clone);
+                var cpt = 0;
+                var tab = []
+                _.forEach(copy, function (o) {
+                    o.cpt = 0
+                    console.log(o.name + " " + o.tags)
+                    if (o.tags) {
+                        console.log(typeof o.tags[0])
+                        for (var i = 0; i < o.tags.length; i++) {
+                            if (user.tags.indexOf(o.tags[i]) != -1) {
+                                console.log("+1 ", o.tags[i] + o.name + " " + user.tags.indexOf(o.tags[i]))
+                                o.cpt++
+                            }
+                        }
+                    }
+                    tab.push(o)
+                })
+                var sorted = _.sortBy(tab, ['cpt'])
+                sorted.reverse()
+                this.setState({
+                    users: sorted
+                })
+            }
+        }
+        else {
+            var sorted = _.sortBy(this.state.users, [what])
+            if (_.isEqual(sorted, this.state.users))
+                sorted.reverse()
+            this.setState({
+                users: sorted
+            })
+        }
+        console.log(this.state.users)
+    }
+
+    rangeAge() {
+        // var ranged = _.filter(this.state.initialUsers, (user) => (user.age >= this.state.filters.age1 && user.age <= this.state.filters.age2));
+        var ranged = this.state.initialUsers.filter(u => u.age >= this.state.filters.age1 && u.age <= this.state.filters.age2)
+        this.setState({
+            users: ranged
+        })
+        console.log(this.state.filters)
+        console.log(this.state.initialUsers)
+    }
+
+    _handleTextFieldChange(e) {
+        this.setState({
+            filters: update(this.state.filters, {[e.target.name]: {$set: e.target.value}}),
+        }, () => {
+            this.rangeAge()
+        });
+    }
+
+    cancelFilters() {
+        var empty = { age1: "", age2: "" }
+        this.setState({
+            filters : empty,
+            users: this.state.initialUsers
+        })
     }
 
     render() {
@@ -43,8 +119,64 @@ export class Browse extends React.Component {
                 )
         });
 
+        const btnStyle = { margin: 10, };
+
         return (
             <div style={{marginBottom: 50, height: '100%'}}>
+
+                <p>Sort by</p>
+                <div className="row" style={{marginBottom: 50}}>
+                    <RaisedButton
+                        label="Age"
+                        primary={true}
+                        style={btnStyle}
+                        disabled={this.state.saveDisabled}
+                        onTouchTap={() => this.sortBy('age')}
+                    />
+                    <RaisedButton
+                        label="Location"
+                        primary={true}
+                        style={btnStyle}
+                        disabled={this.state.saveDisabled}
+                        onTouchTap={() => this.sortBy('location')}
+                    />
+                    <RaisedButton
+                        label="Popularity"
+                        primary={true}
+                        style={btnStyle}
+                        disabled={this.state.saveDisabled}
+                        onTouchTap={() => this.sortBy('popu')}
+                    />
+                    <RaisedButton
+                        label="Commun tags"
+                        primary={true}
+                        style={btnStyle}
+                        disabled={this.state.saveDisabled}
+                        onTouchTap={() => this.sortBy('tags')}
+                    />
+                    <hr />
+                    <p>Filter by</p>
+                    <TextField
+                        floatingLabelText="Age"
+                        name="age1"
+                        value={this.state.filters.age1}
+                        onChange={this._handleTextFieldChange.bind(this)}
+                        style={{width: 40, marginRight: 10}}
+                    />
+                    -
+                    <TextField
+                        floatingLabelText="Age"
+                        name="age2"
+                        value={this.state.filters.age2}
+                        onChange={this._handleTextFieldChange.bind(this)}
+                        style={{width: 40, marginLeft: 10}}
+                    />
+                    <RaisedButton
+                        label="Cancel"
+                        style={btnStyle}
+                        onTouchTap={() => this.cancelFilters()}
+                    />
+                </div>
                 {/*<UserCard/>*/}
                 {users}
             </div>
